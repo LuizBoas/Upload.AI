6d87447f87dfdb451efd072bcdfb89feb9a8ac0f
LuizBoas <luiz.boas@ccc.ufcg.edu.br>
2023-09-17 13:06:07 -0300

    day 01

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,49 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
-function App() {
-  return <h1>oLÁ</h1>;
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">GitHub</Button>
+        </div>
+      </div>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{`{transcription}`}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <Separator />
+        </aside>
+      </main>
+    </div>
+  );
 }
-
-export default App;
